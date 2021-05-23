ce9acf70ceeff630bc7af836ff865745f3961e59
Denis Golovin <ayreon25@gmail.com>
2021-05-23 17:48:25 +0300

    Создание тестового персонажа, его перемещение. Добавлена виртуальная камера.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,8 +1,47 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float speed = 3.0f;
+    private float horizontal;
+    private float vertical;
+    private Rigidbody2D rigidbody2D;
+    private Animator animator;
+    private Vector2 lookDirection = new Vector2(1, 0);
 
+    private void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 position = transform.position;
+        position.x += speed * horizontal * Time.deltaTime;
+        position.y += speed * vertical * Time.deltaTime;
+        
+        rigidbody2D.MovePosition(position);
+    }
 }
