982d33a93bc0f5d40b5e99e505c8ec1bb1d55428
Azay <zaichenkoandrii322@gmail.com>
2021-05-30 21:38:57 +0300

    Добавил tilemap и переработал класс генерации земли под tilemap

--- Assets/Scripts/LandGenerator.cs
+++ Assets/Scripts/LandGenerator.cs
@@ -1,19 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class LandGenerator : MonoBehaviour
 {
     public int height = 10;
     public int width = 10;
     public List<string> listOfNoises = new List<string>(); // created just for cheking  list in editor
-    public Dictionary<Vector3, string> pointsToSpawn = new Dictionary<Vector3, string>();
+    public Dictionary<Vector3Int, string> pointsToSpawn = new Dictionary<Vector3Int, string>();
+    public TileBase[] MountainsTiles;
+    public TileBase[] ForestTiles;
+    public TileBase[] WaterTiles;
     /*
      * Created Dictionary as Vector3 because .transform.position returns Vector3 instead a Vector2
      * so that we can manage it in code in the future 
      */
     private float nx;
     private float ny;
+    private Tilemap tilemap;
 
     private const string WATER = "Water";
     private const string FOREST = "Forest";
@@ -30,9 +35,9 @@ public class LandGenerator : MonoBehaviour
             {
                  nx = (float)x / width - 0.5f;
                  ny = (float)y / height - 0.5f;
-                Vector3 position = new Vector3(x, y, 0);
+                Vector3Int position = new Vector3Int(x, y, 0);
                 float point = Mathf.PerlinNoise(nx * frequency, ny * frequency);
-                string biom = GetTileFromePoint(point);
+                string biom = GetBiomFromPoint(point);
                 pointsToSpawn.Add(position, biom);
                 listOfNoises.Add(pointsToSpawn[position]);
 
@@ -45,14 +50,29 @@ public class LandGenerator : MonoBehaviour
         CalcNoise();
         foreach (var item in pointsToSpawn)
         {
-            // do something with item.Value or item.Key
-            GameObject toInstantiate = floorTiles[Random.Range(0, floorTiles.Length)];
-            GameObject instance = Instantiate(toInstantiate, item.Key, Quaternion.identity) as GameObject;
-            instance.transform.SetParent(this.gameObject.transform);
+            Vector3Int position = item.Key;
+            TileBase tile = GetTileFromBiomType(item.Value);
+            tilemap.SetTile(position, tile);
         }
     }
-
-    private string GetTileFromePoint (float point)
+    private TileBase GetTileFromBiomType(string biomType)
+    {
+        TileBase tile;
+        switch (biomType)
+        {
+            case WATER:
+                tile = WaterTiles[Random.Range(0, WaterTiles.Length)];
+                break;
+            case FOREST:
+                tile = ForestTiles[Random.Range(0, ForestTiles.Length)];
+                break;
+            default:
+                tile = MountainsTiles[Random.Range(0, MountainsTiles.Length)];
+                break;       
+        }
+        return tile;
+    }
+    private string GetBiomFromPoint (float point)
     {
         if (point < 0.2f)
             return WATER;
@@ -66,6 +86,8 @@ public class LandGenerator : MonoBehaviour
 
     private void Awake()
     {
+        GameObject gameobject =  GameObject.Find("Tilemap");
+        tilemap = gameobject.GetComponent<Tilemap>();
         TileSetup();
     }
 }
